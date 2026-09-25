628934a35af0ff431444a2bfd57fca436cb64133
ppetxaii <68050396@kmitl.ac.th>
2026-09-25 11:50:15 +0700

    haha

--- ex00/main.py
+++ ex00/main.py
@@ -8,6 +8,16 @@ R...
 ....\
 """
     checkmate(board)
+    
+#def main():
+#    board = ("-!ZpQcr\n"
+#     "EitsXgO\n"
+#     ".V56hmL\n"
+#      "zojJ%#f\n"
+#     "9$HD@G7\n"
+#     "AkCqMxv\n"
+#     "TWu=KNl")
+#   checkmate(board)
 
 if __name__ == "__main__":
     main()
